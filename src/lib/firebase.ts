898c44939b2466388f9659aa21cf2337c9fa8750
Frameless <frameless.com@gmail.com>
2025-01-29 17:51:13 +0200

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyB_XwdF8yw0D7xyKrEtH_Ns1CXchqt54bI",
+  authDomain: "chromarx-215c8.firebaseapp.com",
+  projectId: "chromarx-215c8",
+  storageBucket: "chromarx-215c8.firebasestorage.app",
+  messagingSenderId: "837855050071",
+  appId: "1:837855050071:web:a9ffa24473d4ea097ae612"
 };
 
 const app = initializeApp(firebaseConfig);
